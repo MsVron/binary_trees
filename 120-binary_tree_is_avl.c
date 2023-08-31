6fd6a0bb4968166ec1babb3a0775dca0a7655ce5
Aya Berroukech <ayagym14@gmail.com>
2023-08-31 20:20:08 +0100

    Is AVL

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -7,19 +7,59 @@
  * @tree: Pointer to the root node of the tree to measure the height
  * Return: The height of the tree
  */
-int height(const binary_tree_t *tree)
+static int height(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    left_height = height(tree->left);
-    right_height = height(tree->right);
+	left_height = height(tree->left);
+	right_height = height(tree->right);
 
-    if (left_height > right_height)
-        return (left_height + 1);
-    return (right_height + 1);
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
+
+/**
+ * is_bst - Checks if a binary tree is a valid Binary Search Tree
+ * @tree: Pointer to the root node of the tree to check
+ * @min: Minimum value for the current subtree
+ * @max: Maximum value for the current subtree
+ * Return: 1 if tree is a valid BST, and 0 otherwise
+ */
+static int is_bst(const binary_tree_t *tree, int min, int max)
+{
+	if (!tree)
+		return (1);
+
+	if (tree->n < min || tree->n > max)
+		return (0);
+
+	return (is_bst(tree->left, min, tree->n - 1) &&
+		is_bst(tree->right, tree->n + 1, max));
+}
+
+/**
+ * check_avl - Checks if a binary tree is a valid AVL Tree
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
+ */
+static int check_avl(const binary_tree_t *tree)
+{
+	int balance_factor;
+
+	if (!tree)
+		return (1);
+
+	if (!is_bst(tree, INT_MIN, INT_MAX))
+		return (0);
+
+	balance_factor = height(tree->left) - height(tree->right);
+
+	if (balance_factor > 1 || balance_factor < -1)
+		return (0);
+
+	return (check_avl(tree->left) && check_avl(tree->right));
 }
 
 /**
@@ -29,27 +69,8 @@ int height(const binary_tree_t *tree)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    int balance_factor;
+	if (!tree)
+		return (0);
 
-    if (!tree)
-        return (0);
-
-    /* Check if it's a BST */
-    if (tree->left && tree->left->n >= tree->n)
-        return (0);
-    if (tree->right && tree->right->n <= tree->n)
-        return (0);
-
-    /* Check balance factor */
-    balance_factor = height(tree->left) - height(tree->right);
-    if (balance_factor > 1 || balance_factor < -1)
-        return (0);
-
-    /* Check left and right subtrees */
-    if (tree->left && !binary_tree_is_avl(tree->left))
-        return (0);
-    if (tree->right && !binary_tree_is_avl(tree->right))
-        return (0);
-
-    return (1);
+	return (check_avl(tree));
 }
