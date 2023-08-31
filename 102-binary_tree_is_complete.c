f2f1bfad49ae10e215ad6aee3157cb9d93d01130
Aya Berroukech <ayagym14@gmail.com>
2023-08-31 19:38:22 +0100

    Is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -2,28 +2,44 @@
 #include <stdlib.h>
 
 /**
- * enqueue - Helper function to enqueue a node into a dynamic array (queue).
- * @queue: Double pointer to the dynamic array (queue).
- * @size: Pointer to the size of the queue.
+ * enqueue - Helper function to enqueue a node.
+ * @queue: The queue.
+ * @size: The size of the queue.
  * @node: The node to enqueue.
  * Return: 1 if successful, 0 otherwise.
  */
 static int enqueue(binary_tree_t ***queue, size_t *size, binary_tree_t *node)
 {
-	/* Reallocate memory for the new size */
-	binary_tree_t **new_queue = realloc(*queue, sizeof(binary_tree_t *) * (*size + 1));
+	binary_tree_t **new_queue;
 
-	/* Check for memory allocation failure */
+	new_queue = realloc(*queue, sizeof(binary_tree_t *) * (*size + 1));
 	if (!new_queue)
 		return (0);
-
-	/* Add the new node to the end of the queue */
 	new_queue[*size] = node;
-
-	/* Update the size and the queue pointer */
 	*size += 1;
 	*queue = new_queue;
+	return (1);
+}
 
+/**
+ * process_node - Processes a node during level-order traversal.
+ * @node: The node to process.
+ * @queue: The queue.
+ * @size: The size of the queue.
+ * @null_seen: Flag indicating if a NULL node has been seen.
+ * Return: 1 if successful, 0 otherwise.
+ */
+static int process_node(binary_tree_t *node, binary_tree_t ***queue,
+                        size_t *size, int *null_seen)
+{
+	int left_enqueue_success, right_enqueue_success;
+
+	if (*null_seen)
+		return (0);
+	left_enqueue_success = enqueue(queue, size, node->left);
+	right_enqueue_success = enqueue(queue, size, node->right);
+	if (!left_enqueue_success || !right_enqueue_success)
+		return (0);
 	return (1);
 }
 
@@ -34,59 +50,33 @@ static int enqueue(binary_tree_t ***queue, size_t *size, binary_tree_t *node)
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	binary_tree_t **queue;  /* Dynamic array to use as a queue */
-	size_t size = 0, i = 0; /* Size of the queue and loop index */
-	int null_seen = 0;      /* Flag to indicate if a NULL child has been seen */
-	int left_enqueue_success, right_enqueue_success; /* Flags for enqueue success */
+	binary_tree_t **queue;
+	size_t size = 0, i = 0;
+	int null_seen = 0;
 
-	/* Check for empty tree */
 	if (!tree)
 		return (0);
-
-	/* Initialize the queue with the root node */
 	queue = malloc(sizeof(binary_tree_t *));
-	if (!queue)
-		return (0);
-	if (!enqueue(&queue, &size, (binary_tree_t *)tree))
+	if (!queue || !enqueue(&queue, &size, (binary_tree_t *)tree))
 		return (0);
 
-	/* Level-order traversal */
 	while (i < size)
 	{
 		binary_tree_t *node = queue[i];
-
-		/* If a node is non-NULL */
 		if (node)
 		{
-			/* If a NULL child has been seen before, the tree is not complete */
-			if (null_seen)
-			{
-				free(queue);
-				return (0);
-			}
-
-			/* Enqueue left and right children */
-			left_enqueue_success = enqueue(&queue, &size, node->left);
-			right_enqueue_success = enqueue(&queue, &size, node->right);
-
-			/* Check for memory allocation failure */
-			if (!left_enqueue_success || !right_enqueue_success)
+			if (!process_node(node, &queue, &size, &null_seen))
 			{
 				free(queue);
 				return (0);
 			}
 		}
-		else /* If a node is NULL */
+		else
 		{
-			/* Set the flag to indicate that a NULL child has been seen */
 			null_seen = 1;
 		}
-
-		/* Move to the next node in the queue */
 		i++;
 	}
-
-	/* Free the queue and return success */
 	free(queue);
 	return (1);
 }
